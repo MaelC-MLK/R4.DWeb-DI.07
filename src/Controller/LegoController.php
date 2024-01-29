e44e0389d185d77faf5adc9f0950dd61148cacd9
Mael Cheron <mael.cheron@etu.unilim.fr>
2024-01-29 15:58:24 +0100

    TP2

--- src/Controller/LegoController.php
+++ src/Controller/LegoController.php
@@ -4,35 +4,70 @@
 /* indique où "vit" ce fichier */
 namespace App\Controller;
 
-
 /* indique l'utilisation du bon bundle pour gérer nos routes */
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use stdClass;
 
+use App\Entity\Lego;
 
 /* le nom de la classe doit être cohérent avec le nom du fichier */
-class TestController extends AbstractController
+class LegoController extends AbstractController
 {
+    private array $legos = [];
+
+    public function __construct()
+    {
+        $data = file_get_contents(__DIR__ . '../../data.json');
+        $legosData = json_decode($data);
+
+        foreach ($legosData as $legoData) {
+            $lego = new Lego(
+                $legoData->id,
+                $legoData->name,
+                $legoData->collection
+            );
+            $lego->setDescription($legoData->description);
+            $lego->setPrice($legoData->price);
+            $lego->setPieces($legoData->pieces);
+            $lego->setBoxImage($legoData->images->box);
+            $lego->setLegoImage($legoData->images->bg);
+
+            $this->legos[] = $lego;
+        }
+    }    
+
+
+
    // L’attribute #[Route] indique ici que l'on associe la route
    // "/" à la méthode home pour que Symfony l'exécute chaque fois
    // que l'on accède à la racine de notre site.
 
     #[Route('/')]
     public function home()
-    {
-        $msg = "Hello World !";
-        return $this->render('test.html.twig', [
-            'msg' => $msg
+    {   
+        $cocci = new stdClass();
+        $cocci->collection = "Creator Expert";
+        $cocci->id = 10252;
+        $cocci->name = "La coccinelle Volkwagen";
+        $cocci->description = "Construis une réplique LEGO® Creator Expert de l'automobile la plus populaire au monde. Ce magnifique modèle LEGO est plein de détails authentiques qui capturent le charme et la personnalité de la voiture, notamment un coloris bleu ciel, des ailes arrondies, des jantes blanches avec des enjoliveurs caractéristiques, des phares ronds et des clignotants montés sur les ailes.";
+        $cocci->price = 94.99;
+        $cocci->pieces = 1167;
+        $cocci->boxImage = "LEGO_10252_Box.png";
+        $cocci->legoImage = "LEGO_10252_Main.jpg";
+
+        return $this->render('/lego.html.twig', [
+            'cocci' => $cocci,
         ]);
     }
 
 
-   #[Route('/me', )]
-   public function Me()
-   {
-       die("Get lost.");
-   }
+
+//    #[Route('/me', )]
+//    public function Me()
+//    {
+//        die("Get lost.");
+//    }
 }
 
 
